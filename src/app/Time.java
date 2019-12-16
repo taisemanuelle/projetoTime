1eb5c35c6e3cc4cf9e556a4a794408ad77e853e4
josyoliveira <joseane.olliveira52@gmail.com>
2019-12-16 15:33:08 -0300

    Criaçao de setters dos atributos: anoCricao, qntSocios e historico

--- src/app/Time.java
+++ src/app/Time.java
@@ -3,34 +3,15 @@ package app;
 public class Time {
     private String nome;
     private String categoria;
-<<<<<<< HEAD
     private int anoCriacao;
     private int qntSocios;
     private String historico;
  
-
-    public void setAnoCriacao(int anoCriacao) {
-        this.anoCriacao = anoCriacao;
-    }
-
-    public void setQntSocios(int qntSocios) {
-        this.qntSocios = qntSocios;
-    }
-
-    public void setHistorico(String historico) {
-        this.historico = historico;
-    }
-
-=======
-    private int anoCriacao = 0;
-    private int qntSocios = 0;
-    private String historico = "indefinido";
-
     public Time(String nome, String categoria) {
         this.nome = nome;
         this.categoria = categoria;
     }
->>>>>>> dbda38d623ef9f005add0c9ef1f89a88ab22ede5
+
 
     public String getNome() {
         return nome;
@@ -51,8 +32,16 @@ public class Time {
     public String getHistorico() {
         return historico;
     }
+    public void setAnoCriacao(int anoCriacao) {
+        this.anoCriacao = anoCriacao;
+    }
 
-    
+    public void setQntSocios(int qntSocios) {
+        this.qntSocios = qntSocios;
+    }
 
-    
+    public void setHistorico(String historico) {
+        this.historico = historico;
+    }
+
 }
