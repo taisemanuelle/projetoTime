e3eac7dd2cbb38c1edc5226b44eaf8bb638efb83
miqueias <domingosmiqueias@gmail.com>
2019-12-16 15:20:14 -0300

    gerando os metodos de acesso get.

--- src/app/Time.java
+++ src/app/Time.java
@@ -6,5 +6,27 @@ public class Time {
     private int anoCriacao;
     private int qntSocios;
     private String historico;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getAnoCriacao() {
+        return anoCriacao;
+    }
+
+    public int getQntSocios() {
+        return qntSocios;
+    }
+
+    public String getHistorico() {
+        return historico;
+    }
+
+    
     
 }
