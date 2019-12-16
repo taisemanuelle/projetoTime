c675f74f0d5fb1f65224e1fe74801544b22b4722
LaisFabianne <lais.fabianne@gmail.com>
2019-12-16 15:21:36 -0300

    Criação do construtor e inicialização dos atributos

--- src/app/Time.java
+++ src/app/Time.java
@@ -3,8 +3,16 @@ package app;
 public class Time {
     private String nome;
     private String categoria;
-    private int anoCriacao;
-    private int qntSocios;
-    private String historico;
+    private int anoCriacao = 0;
+    private int qntSocios = 0;
+    private String historico = "indefinido";
+
+    public Time(String nome, String categoria) {
+        this.nome = nome;
+        this.categoria = categoria;
+    }
+
+
+
     
 }
