233422875830beaae3797b9dcd4bac99c82d1b17
LaisFabianne <lais.fabianne@gmail.com>
2019-12-16 14:35:22 -0300

    Criação da classe time e seus atributos.

---
+++ src/app/Time.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Time {
+    private String nome;
+    private String categoria;
+    private int anoCriacao;
+    private int qntSocios;
+    private String historico;
+    
+}
